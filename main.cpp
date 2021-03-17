cdd1413301d5318a65099fc3e23fce65d547366a
Greenscreen23 <lukas66h@gmail.com>
2021-03-17 14:30:22 +0100

    Intial Commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define rep(a, b)   for(ll a = 0; a < (b); ++a)
+#define frep(b)      while((b)--)
+#define repb(a,b)    for(ll a = (b) - 1; a >= 0; --a)
+#define all(a)      (a).begin();(a).end()
+#define endl        '\n'
+
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+
+void manageCase() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    //input
+    /**
+    int t;
+    cin >> t;
+    frep(t) {
+        manageCase();
+    }*/
+    manageCase();
+
+    return 0;
+}
