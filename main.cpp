93dfd27d8d0eccf3a7da7c3285a3a9d562751c1b
Greenscreen23 <lukas66h@gmail.com>
2021-03-18 23:44:00 +0100

    refactor

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 
-#define rep(a, b)   for(ll a = 0; a < (b); ++a)
-#define frep(b)     while((b)--)
-#define repb(a,b)   for(ll a = (b) - 1; a >= 0; --a)
-#define all(a)      (a).begin(),(a).end()
-#define endl        '\n'
+#define rep(a, b) for (ll a = 0; a < (b); ++a)
+#define frep(b) while ((b)--)
+#define repb(a, b) for (ll a = (b)-1; a >= 0; --a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
 
 using namespace std;
 using Graph = vector<vector<int>>;
 using ll = long long;
 
-void manageCase() {
-
+void manageCase()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
@@ -22,10 +23,11 @@ int main() {
     //input
     int t;
     cin >> t;
-    frep(t) {
+    frep(t)
+    {
         manageCase();
     }
-    //manageCase();
+    manageCase();
 
     return 0;
 }
