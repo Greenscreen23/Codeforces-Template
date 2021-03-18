f855bf41962c827b8bd2823a91f043da8ee3b57d
Greenscreen23 <lukas66h@gmail.com>
2021-03-18 15:11:14 +0100

    cosmetic changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 
 #define rep(a, b)   for(ll a = 0; a < (b); ++a)
-#define frep(b)      while((b)--)
-#define repb(a,b)    for(ll a = (b) - 1; a >= 0; --a)
-#define all(a)      (a).begin();(a).end()
+#define frep(b)     while((b)--)
+#define repb(a,b)   for(ll a = (b) - 1; a >= 0; --a)
+#define all(a)      (a).begin(),(a).end()
 #define endl        '\n'
 
 using namespace std;
@@ -11,7 +11,7 @@ using Graph = vector<vector<int>>;
 using ll = long long;
 
 void manageCase() {
-    
+
 }
 
 int main() {
@@ -20,13 +20,12 @@ int main() {
     cout.precision(10);
 
     //input
-    /**
     int t;
     cin >> t;
     frep(t) {
         manageCase();
-    }*/
-    manageCase();
+    }
+    //manageCase();
 
     return 0;
 }
